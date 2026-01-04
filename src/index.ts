33db811c3ccc443c7d8c4ed5aa8798ccac3431f8
Ayush <ayushkumar85385@gmail.com>
2026-01-04 10:52:38 +0530

    linting

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
-import {connectDB} from './database/connect.js';
-import dotenv from 'dotenv';
+import {connectDB} from "./database/connect.js";
+import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-    origin: process.env.CORS_ORIGIN
-}));
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN
+    })
+);
 app.use(express.json());
 
 // Connect to MongoDB
@@ -23,7 +25,6 @@ app.get("/", (req, res) => {
         message: "RAG Backend is running"
     });
 });
-
 
 // Starting the server
 
